d7906438883ca45dd9e581eba8bb97d7fe300931
Оксана Краснодемская <demskaya01@gmail.com>
2024-11-07 01:24:49 +0300

    add colors to tailwind.config,ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+      },
+      colors: {
+        ozonBlue: '#005BFF',
+        ozonMagenta: '#F1117E',
+        darkSpace: '#001A34',
+        morningBlue: '#00A2FF',
+        greenGrass: '#00BE6C',
+        orangeStar: '#FFA800',
+        gray: '#F2F5F8',
+        white: '#FFFFFF',
       },
     },
   },
